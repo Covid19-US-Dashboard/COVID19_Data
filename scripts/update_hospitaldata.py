31585e94ad34d4d43de7210a5e4b104f8a277ee5
Tianning Li <51225681+lit26@users.noreply.github.com>
2021-02-13 13:39:01 -0800

    Update update_hospitaldata.py

--- scripts/update_hospitaldata.py
+++ scripts/update_hospitaldata.py
@@ -8,25 +8,35 @@ import glob
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) \
             AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'}
 
+def get_header(link_url):
+    link_file = link_url.split('files/')[1]
+    df = pd.read_csv(link_url)
+    if link_file.startswith('estimated_inpatient_all'):
+        df = df[['state', 'collection_date', 'Inpatient Beds Occupied Estimated',
+                'Percentage of Inpatient Beds Occupied Estimated', 'Total Inpatient Beds']]
+    elif link_file.startswith('estimated_inpatient_covid'):
+        df = df[['state', 'collection_date', 'Inpatient Beds Occupied by COVID-19 Patients Estimated',
+               'Percentage of Inpatient Beds Occupied by COVID-19 Patients Estimated']]
+    else:
+        df = df[['state', 'collection_date', 'Staffed Adult ICU Beds Occupied Estimated',
+             'Percentage of Staffed Adult ICU Beds Occupied Estimated', 'Total Staffed Adult ICU Beds']]
+    return df
+
 def get_data():
     url = 'https://healthdata.gov/dataset/covid-19-estimated-patient-impact-and-hospital-capacity-state'
     source = requests.get(url, headers=headers)
 
     # get html
     soup = BeautifulSoup(source.text, "html.parser")
-
     resources = soup.find(id='data-and-resources').findChildren()[1]
     link_row = resources.findAll('span', class_="links")
+    link_url = link_row[0].findAll('a')[1].attrs['href']
+    df1 = get_header(link_url)
+    link_url = link_row[1].findAll('a')[1].attrs['href']
+    df2 = get_header(link_url)
+    link_url = link_row[2].findAll('a')[1].attrs['href']
+    df3 = get_header(link_url)
 
-    df1 = pd.read_csv(link_row[0].findAll('a')[1].attrs['href'])
-    df2 = pd.read_csv(link_row[1].findAll('a')[1].attrs['href'])
-    df3 = pd.read_csv(link_row[2].findAll('a')[1].attrs['href'])
-    df2 = df2[['state','collection_date','Inpatient Beds Occupied Estimated',
-               'Percentage of Inpatient Beds Occupied Estimated','Total Inpatient Beds']]
-    df1 = df1[['state','collection_date','Inpatient Beds Occupied by COVID-19 Patients Estimated',
-               'Percentage of Inpatient Beds Occupied by COVID-19 Patients Estimated']]
-    df3 = df3[['state','collection_date','Staffed Adult ICU Beds Occupied Estimated',
-               'Percentage of Staffed Adult ICU Beds Occupied Estimated','Total Staffed Adult ICU Beds']]
     df = df1.merge(df2, on=['state','collection_date']).merge(df3, on=['state','collection_date'])
     return df
 
